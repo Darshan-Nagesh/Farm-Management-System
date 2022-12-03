6e511c6c6d3e7a62e563f27294e58f222d082fec
Darshan-Nagesh <117649821+Darshan-Nagesh@users.noreply.github.com>
2022-12-03 12:31:04 +0530

    Update farm.sql

--- farm.sql
+++ farm.sql
@@ -1,65 +1,80 @@
-CREATE DATABASE FARM_MANAGE;
-USE FARM_MANAGE;
-CREATE TABLE login(
-    login_id INTEGER PRIMARY KEY,
-    login_role_id INTEGER,
-    login_username VARCHAR(20),
-    login_password VARCHAR(12)
+CREATE DATABASE FARM_MANAGEMENT;
+USE FARM_MANAGEMENT;
+
+CREATE TABLE users(
+   user_id INTEGER PRIMARY KEY,
+   user_name VARCHAR(20),
+   user_phone  LONG,
+   user_email VARCHAR(20),
+   user_add VARCHAR(30)
 );
-CREATE TABLE users_(
-    user_id INTEGER PRIMARY KEY,
-    user_role_id INTEGER,
-    user_name VARCHAR(20),
-    user_email VARCHAR(15),
-    user_phone VARCHAR(10),
-    user_add TEXT
-);
+
 CREATE TABLE roles(
     role_id INTEGER PRIMARY KEY,
+    per_id INTEGER PRIMARY KEY,
     role_name VARCHAR(20),
-    role_desc TEXT
+    role_desc TEXT,
+    per_desc TEXT
 );
-CREATE TABLE permission(
-    per_id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    per_role_id INTEGER,
-    per_name VARCHAR(20),
-    per_module TEXT,
-    per_desc TEXT,
-    FOREIGN KEY per_role_id REFERENCES roles(role_id)
+
+CREATE TABLE login(
+    login_id INTEGER,
+    login_role_id INTEGER,
+    login_username VARCHAR(20),
+    user_password VARCHAR(12),
+    login_date DATE,
+    FOREIGN KEY (login_role_id) REFERENCES roles(role_id)
 );
+
 CREATE TABLE employee(
-    emp_id INTEGER PRIMARY KEY,
-    emp_name VARCHAR(20),
-    emp_username VARCHAR(20),
-    emp_password VARCHAR(12),
-    emp_email VARCHAR(15),
-    emp_phone VARCHAR(10),
-    emp_add TEXT
-);    
+   emp_id INTEGER PRIMARY KEY,
+   emp_name VARCHAR(20),
+   emp_email VARCHAR(20),
+   emp_phone LONG,
+   emp_add VARCHAR(30),
+   emp_username VARCHAR(20),
+   emp_password VARCHAR(15)
+);
+
+CREATE TABLE plant(
+    plant_id INTEGER PRIMARY KEY,
+    plant_name VARCHAR(20),
+    plant_type VARCHAR(20),
+    plant_desc TEXT,
+    soil_type VARCHAR(20)
+);
 CREATE TABLE medicines(
+    plant_id INTEGER,
     med_id INTEGER PRIMARY KEY,
     med_name VARCHAR(20),
     med_type VARCHAR(20),
     med_cost INTEGER,
     med_desc TEXT,
-    FOREIGN KEY  REFERENCES farm(farm_id)
+    FOREIGN KEY (plant_id) REFERENCES plant(plant_id)
 );
-CREATE TABLE farm(
-    farm_id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    farm_type VARCHAR(20),
-    farm_desc TEXT
+CREATE TABLE method(
+    method_id INTEGER PRIMARY KEY,
+    method_name VARCHAR(20),
+    method_type VARCHAR(20),
+    method_desc TEXT
 );
-CREATE TABLE treatment(
-    treat_id INTEGER,
-    treat_name VARCHAR(20),
-    treat_type VARCHAR(20),
-    treat_method TEXT,
-    treat_desc TEXT,
-    FOREIGN KEY treat_id REFERENCES farm(farm_id)
+CREATE TABLE has(
+    user_id INTEGER,
+    login_id INTEGER,
+    role_id INTEGER,
+    per_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (role_id) REFERENCES roles(role_id),
+    FOREIGN KEY (login_id) REFERENCES login(login_id),
+    FOREIGN KEY (per_id) REFERENCES roles(per_id)
 );
-CREATE TABLE plant(
-    plant_id INTEGER,
-    plant_name VARCHAR(20),
-    plant_type VARCHAR(20),
-    FOREIGN KEY plant_id REFERENCES farm(farm_id)
+CREATE TABLE manages(
+   user_id INTEGER,
+   emp_id INTEGER,
+   plant_id INTEGER,
+   method_id INTEGER,
+   FOREIGN KEY (user_id) REFERENCES users(user_id),
+   FOREIGN KEY (emp_id) REFERENCES employee(emp_id),
+   FOREIGN KEY (plant_id) REFERENCES login(plant_id),
+   FOREIGN KEY (method_id) REFERENCES method(method_id)
 );
