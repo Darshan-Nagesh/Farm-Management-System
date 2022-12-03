0a133a34291b77bc63bfdb08bc0e431ea5892506
Darshan-Nagesh <117649821+Darshan-Nagesh@users.noreply.github.com>
2022-12-03 12:54:19 +0530

    Update farm.sql

--- farm.sql
+++ farm.sql
@@ -8,23 +8,25 @@ CREATE TABLE users(
    user_email VARCHAR(20),
    user_add VARCHAR(30)
 );
+DESC users;
 
 CREATE TABLE roles(
     role_id INTEGER PRIMARY KEY,
-    per_id INTEGER PRIMARY KEY,
     role_name VARCHAR(20),
     role_desc TEXT,
     per_desc TEXT
 );
+DESC roles;
 
 CREATE TABLE login(
-    login_id INTEGER,
+    login_id INTEGER PRIMARY KEY,
     login_role_id INTEGER,
     login_username VARCHAR(20),
     user_password VARCHAR(12),
     login_date DATE,
     FOREIGN KEY (login_role_id) REFERENCES roles(role_id)
 );
+DESC login;
 
 CREATE TABLE employee(
    emp_id INTEGER PRIMARY KEY,
@@ -35,6 +37,7 @@ CREATE TABLE employee(
    emp_username VARCHAR(20),
    emp_password VARCHAR(15)
 );
+DESC employee;
 
 CREATE TABLE plant(
     plant_id INTEGER PRIMARY KEY,
@@ -43,6 +46,8 @@ CREATE TABLE plant(
     plant_desc TEXT,
     soil_type VARCHAR(20)
 );
+DESC plant;
+
 CREATE TABLE medicines(
     plant_id INTEGER,
     med_id INTEGER PRIMARY KEY,
@@ -52,22 +57,26 @@ CREATE TABLE medicines(
     med_desc TEXT,
     FOREIGN KEY (plant_id) REFERENCES plant(plant_id)
 );
+DESC medicines;
+
 CREATE TABLE method(
     method_id INTEGER PRIMARY KEY,
     method_name VARCHAR(20),
     method_type VARCHAR(20),
     method_desc TEXT
 );
+DESC method;
+
 CREATE TABLE has(
     user_id INTEGER,
     login_id INTEGER,
     role_id INTEGER,
-    per_id INTEGER,
     FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (role_id) REFERENCES roles(role_id),
     FOREIGN KEY (login_id) REFERENCES login(login_id),
-    FOREIGN KEY (per_id) REFERENCES roles(per_id)
+    FOREIGN KEY (role_id) REFERENCES roles(role_id)
 );
+DESC has;
+
 CREATE TABLE manages(
    user_id INTEGER,
    emp_id INTEGER,
@@ -75,6 +84,7 @@ CREATE TABLE manages(
    method_id INTEGER,
    FOREIGN KEY (user_id) REFERENCES users(user_id),
    FOREIGN KEY (emp_id) REFERENCES employee(emp_id),
-   FOREIGN KEY (plant_id) REFERENCES login(plant_id),
+   FOREIGN KEY (plant_id) REFERENCES plant(plant_id),
    FOREIGN KEY (method_id) REFERENCES method(method_id)
 );
+DESC manages;
